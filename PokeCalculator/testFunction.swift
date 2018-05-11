a3447fd9b9b564d81b5c233f3cf9226927e99b8d
JordanWilli <jkw12a@acu.edu>
2018-05-11 01:48:39 -0500

    First commit. And testing header file

---
+++ PokeCalculator/testFunction.swift
@@ -0,0 +1,14 @@
+//
+//  testFunction.swift
+//  PokeCalculator
+//
+//  Created by Jordan Williams on 5/10/18.
+//  Copyright © 2018 Jordan Williams. All rights reserved.
+//
+
+import Foundation
+
+func addME(a: Int, b:Int) -> Int
+{
+    return a+b;
+}
